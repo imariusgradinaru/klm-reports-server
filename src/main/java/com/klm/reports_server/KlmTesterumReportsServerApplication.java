487969f41e1476dfedfb5082a19328622f841b2d
Marius Gradinaru <imariusgradinaru@gmail.com>
2021-03-02 22:49:23 +0100

    Initial commit

---
+++ src/main/java/com/klm/reports_server/KlmTesterumReportsServerApplication.java
@@ -0,0 +1,13 @@
+package com.klm.reports_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlmTesterumReportsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlmTesterumReportsServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/klm/reports_server/KlmTesterumReportsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klm.reports_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlmTesterumReportsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
